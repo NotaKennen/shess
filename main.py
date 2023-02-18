2c4a36a46f275049386783c419590f456add5e44
BuildTools <unconfigured@null.spigotmc.org>
2023-02-18 17:49:32 +0200

    Added removing pieces and blocking removing own color pieces, also fixes on movement

--- main.py
+++ main.py
@@ -209,48 +209,176 @@ def convertValue(unit, value): # Units: "pixels"/"squares" /\ Value: (just any v
 
 def drawpieces():
     # Pawns
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn1.xpos), convertValue("pixels", b_pawn1.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn2.xpos), convertValue("pixels", b_pawn2.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn3.xpos), convertValue("pixels", b_pawn3.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn4.xpos), convertValue("pixels", b_pawn4.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn5.xpos), convertValue("pixels", b_pawn5.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn6.xpos), convertValue("pixels", b_pawn6.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn7.xpos), convertValue("pixels", b_pawn7.ypos)))
-    scrn.blit(b_pawn, (convertValue("pixels", b_pawn8.xpos), convertValue("pixels", b_pawn8.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn1.xpos), convertValue("pixels", w_pawn1.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn2.xpos), convertValue("pixels", w_pawn2.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn3.xpos), convertValue("pixels", w_pawn3.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn4.xpos), convertValue("pixels", w_pawn4.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn5.xpos), convertValue("pixels", w_pawn5.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn6.xpos), convertValue("pixels", w_pawn6.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn7.xpos), convertValue("pixels", w_pawn7.ypos)))
-    scrn.blit(w_pawn, (convertValue("pixels", w_pawn8.xpos), convertValue("pixels", w_pawn8.ypos)))
+    if b_pawn1.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn1.xpos), convertValue("pixels", b_pawn1.ypos)))
+    else:
+        b_pawn1.xpos = 9
+        b_pawn1.ypos = 9
+    if b_pawn2.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn2.xpos), convertValue("pixels", b_pawn2.ypos)))
+    else:
+        b_pawn2.xpos = 9
+        b_pawn2.ypos = 9
+    if b_pawn3.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn3.xpos), convertValue("pixels", b_pawn3.ypos)))
+    else:
+        b_pawn3.xpos = 9
+        b_pawn3.ypos = 9
+    if b_pawn4.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn4.xpos), convertValue("pixels", b_pawn4.ypos)))
+    else:
+        b_pawn4.xpos = 9
+        b_pawn4.ypos = 9
+    if b_pawn5.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn5.xpos), convertValue("pixels", b_pawn5.ypos)))
+    else:
+        b_pawn5.xpos = 9
+        b_pawn5.ypos = 9
+    if b_pawn6.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn6.xpos), convertValue("pixels", b_pawn6.ypos)))
+    else:
+        b_pawn6.xpos = 9
+        b_pawn6.ypos = 9
+    if b_pawn7.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn7.xpos), convertValue("pixels", b_pawn7.ypos)))
+    else:
+        b_pawn7.xpos = 9
+        b_pawn7.ypos = 9
+    if b_pawn8.status == True:
+        scrn.blit(b_pawn, (convertValue("pixels", b_pawn8.xpos), convertValue("pixels", b_pawn8.ypos)))
+    else:
+        b_pawn8.xpos = 9
+        b_pawn8.ypos = 9
+    if w_pawn1.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn1.xpos), convertValue("pixels", w_pawn1.ypos)))
+    else:
+        w_pawn1.xpos = 9
+        w_pawn1.ypos = 9
+    if w_pawn2.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn2.xpos), convertValue("pixels", w_pawn2.ypos)))
+    else:
+        w_pawn2.xpos = 9
+        w_pawn2.ypos = 9
+    if w_pawn3.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn3.xpos), convertValue("pixels", w_pawn3.ypos)))
+    else:
+        w_pawn3.xpos = 9
+        w_pawn3.ypos = 9
+    if w_pawn4.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn4.xpos), convertValue("pixels", w_pawn4.ypos)))
+    else:
+        w_pawn4.xpos = 9
+        w_pawn4.ypos = 9
+    if w_pawn5.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn5.xpos), convertValue("pixels", w_pawn5.ypos)))
+    else:
+        w_pawn5.xpos = 9
+        w_pawn5.ypos = 9
+    if w_pawn6.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn6.xpos), convertValue("pixels", w_pawn6.ypos)))
+    else:
+        w_pawn6.xpos = 9
+        w_pawn6.ypos = 9
+    if w_pawn7.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn7.xpos), convertValue("pixels", w_pawn7.ypos)))
+    else:
+        w_pawn7.xpos = 9
+        w_pawn7.ypos = 9
+    if w_pawn8.status == True:
+        scrn.blit(w_pawn, (convertValue("pixels", w_pawn8.xpos), convertValue("pixels", w_pawn8.ypos)))
+    else:
+        w_pawn8.xpos = 9
+        w_pawn8.ypos = 9
 
     # Rooks
-    scrn.blit(w_rook, (convertValue("pixels", w_rook1.xpos), convertValue("pixels", w_rook1.ypos)))
-    scrn.blit(w_rook, (convertValue("pixels", w_rook2.xpos), convertValue("pixels", w_rook2.ypos)))
-    scrn.blit(b_rook, (convertValue("pixels", b_rook1.xpos), convertValue("pixels", b_rook1.ypos)))
-    scrn.blit(b_rook, (convertValue("pixels", b_rook2.xpos), convertValue("pixels", b_rook2.ypos)))
+    if b_rook1.status == True:
+        scrn.blit(b_rook, (convertValue("pixels", w_rook1.xpos), convertValue("pixels", b_rook1.ypos)))
+    else:
+        b_rook1.xpos = 9
+        b_rook1.ypos = 9
+    if b_rook2.status == True:
+        scrn.blit(b_rook, (convertValue("pixels", w_rook2.xpos), convertValue("pixels", b_rook2.ypos)))
+    else:
+        b_rook2.xpos = 9
+        b_rook2.ypos = 9
+    if w_rook1.status == True:
+        scrn.blit(w_rook, (convertValue("pixels", w_rook1.xpos), convertValue("pixels", w_rook1.ypos)))
+    else:
+        w_rook1.xpos = 9
+        w_rook1.ypos = 9
+    if w_rook2.status == True:
+        scrn.blit(w_rook, (convertValue("pixels", w_rook2.xpos), convertValue("pixels", w_rook2.ypos)))
+    else:
+        w_rook2.xpos = 9
+        w_rook2.ypos = 9
 
     # Bishops
-    scrn.blit(w_bishop, (convertValue("pixels", w_bishop1.xpos), convertValue("pixels", w_bishop1.ypos)))
-    scrn.blit(w_bishop, (convertValue("pixels", w_bishop2.xpos), convertValue("pixels", w_bishop2.ypos)))
-    scrn.blit(b_bishop, (convertValue("pixels", b_bishop1.xpos), convertValue("pixels", b_bishop1.ypos)))
-    scrn.blit(b_bishop, (convertValue("pixels", b_bishop2.xpos), convertValue("pixels", b_bishop2.ypos)))
+    if b_bishop1.status == True:
+        scrn.blit(b_bishop, (convertValue("pixels", b_bishop1.xpos), convertValue("pixels", b_bishop1.ypos)))
+    else:
+        b_bishop1.xpos = 9
+        b_bishop1.ypos = 9
+    if b_bishop2.status == True:
+        scrn.blit(b_bishop, (convertValue("pixels", b_bishop2.xpos), convertValue("pixels", b_bishop2.ypos)))
+    else:
+        b_bishop2.xpos = 9
+        b_bishop2.ypos = 9
+    if w_bishop1.status == True:
+        scrn.blit(w_bishop, (convertValue("pixels", w_bishop1.xpos), convertValue("pixels", w_bishop1.ypos)))
+    else:
+        w_bishop1.xpos = 9
+        w_bishop1.ypos = 9
+    if w_bishop2.status == True:
+        scrn.blit(w_bishop, (convertValue("pixels", w_bishop2.xpos), convertValue("pixels", w_bishop2.ypos)))
+    else:
+        w_bishop2.xpos = 9
+        w_bishop2.ypos = 9
 
     # Knights
-    scrn.blit(w_knight, (convertValue("pixels", w_knight1.xpos), convertValue("pixels", w_knight1.ypos)))
-    scrn.blit(w_knight, (convertValue("pixels", w_knight2.xpos), convertValue("pixels", w_knight2.ypos)))
-    scrn.blit(b_knight, (convertValue("pixels", b_knight1.xpos), convertValue("pixels", b_knight1.ypos)))
-    scrn.blit(b_knight, (convertValue("pixels", b_knight2.xpos), convertValue("pixels", b_knight2.ypos)))
+    if b_knight1.status == True:
+        scrn.blit(b_knight, (convertValue("pixels", b_knight1.xpos), convertValue("pixels", b_knight1.ypos)))
+    else:
+        b_knight1.xpos = 9
+        b_knight1.ypos = 9
+    if b_knight2.status == True:
+        scrn.blit(b_knight, (convertValue("pixels", b_knight2.xpos), convertValue("pixels", b_knight2.ypos)))
+    else:
+        b_knight2.xpos = 9
+        b_knight2.ypos = 9
+    if w_knight1.status == True:
+        scrn.blit(w_knight, (convertValue("pixels", w_knight1.xpos), convertValue("pixels", w_knight1.ypos)))
+    else:
+        w_knight1.xpos = 9
+        w_knight1.ypos = 9
+    if w_knight2.status == True:
+        scrn.blit(w_knight, (convertValue("pixels", w_knight2.xpos), convertValue("pixels", w_knight2.ypos)))
+    else:
+        w_knight2.xpos = 9
+        w_knight2.ypos = 9
 
     # Kings
-    scrn.blit(w_king, (convertValue("pixels", w_king1.xpos), convertValue("pixels", w_king1.ypos)))
-    scrn.blit(b_king, (convertValue("pixels", b_king1.xpos), convertValue("pixels", b_king1.ypos)))
+    if b_king1.status == True:
+        scrn.blit(b_king, (convertValue("pixels", b_king1.xpos), convertValue("pixels", b_king1.ypos)))
+    else:
+        b_king1.xpos = 9
+        b_king1.ypos = 9
+    if w_king1.status == True:
+        scrn.blit(w_king, (convertValue("pixels", w_king1.xpos), convertValue("pixels", w_king1.ypos)))
+    else:
+        w_king1.xpos = 9
+        w_king1.ypos = 9
 
     # Queens
-    scrn.blit(w_queen, (convertValue("pixels", w_queen1.xpos), convertValue("pixels", w_queen1.ypos)))
-    scrn.blit(b_queen, (convertValue("pixels", b_queen1.xpos), convertValue("pixels", b_queen1.ypos)))
+    if b_queen1.status == True:
+        scrn.blit(b_queen, (convertValue("pixels", b_queen1.xpos), convertValue("pixels", b_queen1.ypos)))
+    else:
+        b_queen1.xpos = 9
+        b_queen1.ypos = 9
+    if w_queen1.status == True:
+        scrn.blit(w_queen, (convertValue("pixels", w_queen1.xpos), convertValue("pixels", w_queen1.ypos)))
+    else:
+        w_queen1.xpos = 9
+        w_queen1.ypos = 9
 
 def draw():
     # Draw board
@@ -265,22 +393,28 @@ def draw():
 def reset():
     pass
 
-def canMove(piece: str, current: tuple, to: tuple, remove: bool=True): # Moves a piece from one square to another
+def canMove(piece: str, current: tuple, to: tuple): # Checks if a piece can move from one square to another
     tox = to[0]
     toy = to[1]
     currentx = current[0]
     currenty = current[1]
+    currentColor = piece[0]
+    toColor = getPiece(to[0], to[1])
+    if toColor is None:
+        toColor = "this is useless"
+    toColor = toColor[0]
+    toColor = toColor[0]
 
-    current = getPiece(current[0], current[1])
-    to = getPiece(to[0], to[1])
-    movementStyle = current[1]
+    movementStyle = piece
     piece = current[0]
 
     # Checks for own piece
-    
+    if currentColor[0] == toColor[0]:
+        return False
     # Checks for other pieces
-    if to is not None and remove == True:
-        pass # TODO: Remove piece from board
+    to = getPiece(to[0], to[1])
+    if to is not None:
+        remove(nameToClass(to[0]))
 
     # Bishops
     if movementStyle == "bishop":
@@ -602,7 +736,80 @@ def move(piece, x: int, y: int):
         w_pawn8.xpos = x
         w_pawn8.ypos = y
 
+def remove(piece):
+    # Bishops
+    if piece == b_bishop1:
+        b_bishop1.status = False
+    elif piece == b_bishop2:
+        b_bishop2.status = False
+    elif piece == w_bishop1:
+        w_bishop1.status = False
+    elif piece == w_bishop2:
+        w_bishop2.status = False
+
+    # Rooks
+    elif piece == b_rook1:
+        b_rook1.status = False
+    elif piece == b_rook2:
+        b_rook2.status = False
+    elif piece == w_rook1:
+        w_rook1.status = False
+    elif piece == w_rook2:
+        w_rook2.status = False
+
+    # Knights
+    elif piece == b_knight1:
+        b_knight1.status = False
+    elif piece == b_knight2:
+        b_knight2.status = False
+    elif piece == w_knight1:
+        w_knight1.status = False
+    elif piece == w_knight2:
+        w_knight2.status = False
+
+    elif piece == b_queen1:
+        b_queen1.status = False
+    elif piece == w_queen1:
+        w_queen1.status = False
     
+    elif piece == b_king1:
+        b_king1.status = False
+    elif piece == w_king1:
+        w_king1.status = False
+
+    elif piece == b_pawn1:
+        b_pawn1.status = False
+    elif piece == b_pawn2:
+        b_pawn2.status = False
+    elif piece == b_pawn3:
+        b_pawn3.status = False
+    elif piece == b_pawn4:
+        b_pawn4.status = False
+    elif piece == b_pawn5:
+        b_pawn5.status = False
+    elif piece == b_pawn6:
+        b_pawn6.status = False
+    elif piece == b_pawn7:
+        b_pawn7.status = False
+    elif piece == b_pawn8:
+        b_pawn8.status = False
+    elif piece == w_pawn1:
+        w_pawn1.status = False
+    elif piece == w_pawn2:
+        w_pawn2.status = False
+    elif piece == w_pawn3:
+        w_pawn3.status = False
+    elif piece == w_pawn4:
+        w_pawn4.status = False
+    elif piece == w_pawn5:
+        w_pawn5.status = False
+    elif piece == w_pawn6:
+        w_pawn6.status = False
+    elif piece == w_pawn7:
+        w_pawn7.status = False
+    elif piece == w_pawn8:
+        w_pawn8.status = False
+
 ######################## Draw screen one time 
 pygame.display.flip()
 ######################## Game loop
@@ -639,11 +846,15 @@ while (active):
             mouseX = convertValue("squares", mousepos[0])
             mouseY = convertValue("squares", mousepos[1])
             if piece is not None:
-                error = canMove(piece[0], (oldmouseX, oldmouseY), (mouseX, mouseY))
+                error = canMove(piece[1], (oldmouseX, oldmouseY), (mouseX, mouseY))
                 if error == False: 
                     print("Invalid move")
                 else: # Do the move
                     move(nameToClass(piece[0]), mouseX, mouseY)
+                moveclick = False
+                mouseX = 8
+                mouseY = 8
+                mousepos = None
                 moveclick = False
         else:
             mouseX = convertValue("squares", mousepos[0])
